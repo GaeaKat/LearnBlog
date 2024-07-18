bae0b03978aab2e9e16dcf9ee945917ef6f6b3a6
Kat Gaea <kat.knight@newgaea.net>
2024-07-18 12:17:15 +0100

    feat: add a load of stuff

---
+++ backend/src/routes/posts.js
@@ -0,0 +1,73 @@
+import {
+  listAllPosts,
+  listPostsByAuthor,
+  listPostsByTag,
+  getPostById,
+  createPost,
+  updatePost,
+  deletePost,
+} from '../services/posts.js'
+
+export function postsRoutes(app) {
+  app.get('/api/v1/posts', async (req, res) => {
+    const { sortBy, sortOrder, author, tag } = req.query
+    const options = { sortBy, sortOrder }
+    try {
+      if (author && tag) {
+        return res
+          .status(400)
+          .json({ error: 'query by either author or tag, not both' })
+      } else if (author) {
+        return res.json(await listPostsByAuthor(author, options))
+      } else if (tag) {
+        return res.json(await listPostsByTag(tag, options))
+      } else {
+        return res.json(await listAllPosts(options))
+      }
+    } catch (err) {
+      console.error('Error getting posts:', err)
+      return res.status(500).json({ error: 'Error getting posts' })
+    }
+  })
+
+  app.get('/api/v1/posts/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+      const post = await getPostById(id)
+      if (post == null) return res.status(404).json({ error: 'Post not found' })
+      return res.json(post)
+    } catch (err) {
+      console.error('Error getting post:', err)
+      return res.status(500).json({ error: 'Error getting post' })
+    }
+  })
+
+  app.post('/api/v1/posts', async (req, res) => {
+    try {
+      const post = await createPost(req.body)
+      return res.json(post)
+    } catch (err) {
+      console.error('error creating post', err)
+      return res.status(500).json({ error: 'Error creating post' })
+    }
+  })
+  app.patch('/api/v1/posts/:id', async (req, res) => {
+    try {
+      const post = await updatePost(req.params.id, req.body)
+      return res.json(post)
+    } catch (err) {
+      console.error('error updating post', err)
+      return res.status(500).json({ error: 'Error updating post' })
+    }
+  })
+  app.delete('/api/v1/posts/:id', async (req, res) => {
+    try {
+      const { deletedCount } = await deletePost(req.params.id)
+      if (deletedCount === 0) return res.sendStatus(404)
+      return res.status(204).end()
+    } catch (err) {
+      console.error('error deleting post', err)
+      return res.status(500).json({ error: 'Error deleting post' })
+    }
+  })
+}
